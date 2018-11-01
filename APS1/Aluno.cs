d689b97aa72079d811aa8f9aaeadd0976297dd9f
fabianobf <37447529+fabianobf@users.noreply.github.com>
2018-11-01 14:04:59 -0300

    Segunda questão

---
+++ APS1/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS1 {
+    class Aluno {
+        string NomeCompleto;
+        long Matricula;
+        string Cpf;
+        public Aluno (string nome, long matricula) {
+            NomeCompleto = nome;
+            Matricula = matricula;
+        }
+        public Aluno (string nome, string cpf) {
+            NomeCompleto = nome;
+            Cpf = cpf;
+        }
+        public Aluno() { }
+        public void setNome(string nome) {
+            NomeCompleto = nome;
+        }
+        public void setMatricula(long matricula) {
+            Matricula = matricula;
+        }
+        public void setCpf(string cpf) {
+            Cpf = cpf;
+        }
+        public void imprimirNome() {
+            Console.WriteLine($"Nome: {NomeCompleto}");
+        }
+        public void imprimirMatricula() {
+            Console.WriteLine($"Matricula: {Matricula}");
+        }
+        public void imprimeCPF() {
+            Console.WriteLine($"CPF: {Cpf}");
+        }
+    }
+}
---
+++ APS1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS1 {
+    class Program {
+        static void Main(string[] args) {
+            Aluno jose = new Aluno();
+            jose.setNome("Jose Maria");
+            jose.setMatricula(123456);
+            jose.setCpf("123.456.789-98");
+            jose.imprimirNome();
+            jose.imprimirMatricula();
+            jose.imprimeCPF();
+
+            Aluno maria = new Aluno("Maria José", 654321);
+            //Aluno maria = new Aluno("Maria José", "987.654.321-21");
+            maria.setCpf("987.654.321-21");
+            maria.imprimirNome();
+            maria.imprimirMatricula();
+            maria.imprimeCPF();
+            
+            Console.ReadKey();
+        }
+    }
+}
