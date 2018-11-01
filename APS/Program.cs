dc0489ec35e9884bdc91fc248435e3247b47f4fa
fabianobf <37447529+fabianobf@users.noreply.github.com>
2018-11-01 12:00:29 -0300

    Update Program.cs

--- APS/Program.cs
+++ APS/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace APS
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace APS {
+    class Program {
+        static void Main(string[] args) {
             Aluno jose = new Aluno();
+            jose.setNome("Jose Maria");
+            jose.setMatricula(123456);
+            jose.setCpf("123.456.789-98");
+            jose.imprimirNome();
+            jose.imprimirMatricula();
+            jose.imprimeCPF();
+            Console.ReadKey();
         }
     }
 }
