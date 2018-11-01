a19acc78e43dbeef78db85349d253a057f693f76
fabianobf <37447529+fabianobf@users.noreply.github.com>
2018-11-01 16:41:27 -0300

    Questão 3 e 4
    segue questoes 3 e 4

---
+++ Moto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS1 {
+    class Moto {
+        string marca;
+        string modelo;
+        string cor;
+        int marcha = 0;
+        public string denominacaoMarcha = null;
+        public void Imprimir() {
+            Console.WriteLine($"Marca: {marca} Modelo: {modelo} Cor: {cor} e está na {MarchaExtenso()} marcha");
+        }
+        public string MarchaExtenso() {
+            if (marcha == 0) {
+                denominacaoMarcha = "neutro";
+            } else if (marcha == 1) {
+                denominacaoMarcha = "primeira";
+            } else if (marcha == 2) {
+                denominacaoMarcha = "segunda";
+            } else if (marcha == 3) {
+                denominacaoMarcha = "terceira";
+            } else if (marcha == 4) {
+                denominacaoMarcha = "quarta";
+            } else {
+                Console.WriteLine("Não foi definida uma marcha corretamente");
+            }
+            return denominacaoMarcha;
+        }
+        public Moto(string marca, string modelo, string cor, int marcha) {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.cor = cor;
+            this.marcha = marcha;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS1 {
+    class Program {
+        static void Main(string[] args) {
+            Aluno jose = new Aluno();
+            jose.setNome("Jose Maria");
+            jose.setMatricula(123456);
+            jose.setCpf("123.456.789-98");
+            jose.imprimirNome();
+            jose.imprimirMatricula();
+            jose.imprimeCPF();
+
+            Aluno maria = new Aluno("Maria José", 654321);
+            //Aluno maria = new Aluno("Maria José", "987.654.321-21");
+            maria.setCpf("987.654.321-21");
+            maria.imprimirNome();
+            maria.imprimirMatricula();
+            maria.imprimeCPF();
+
+            Moto teste = new Moto("honda", "cb300", "verde", 4);
+            Moto vespa = new Moto("xingling", "vainessa", "burroquandofoge", 1);
+            teste.Imprimir();
+            vespa.Imprimir();
+
+            Console.ReadKey();
+        }
+    }
+}
